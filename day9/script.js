1df1a0db8ffb085a5e12a77551a02829c47d1e69
AkshayRao11 <akshay.khola.11@gmail.com>
2026-02-03 03:50:27 +0530

    Task Changes

---
+++ day9/script.js
@@ -0,0 +1,24 @@
+const task1 = document.getElementById("profile");
+task1.style.backgroundColor="yellow";
+task1.style.padding="15px";
+task1.style.textAlign="center";
+
+
+
+const task2 = document.getElementsByClassName("class2");
+console.log(task2)
+
+for (i=0;i<task2.length;i++){
+  task2[i].style.color="red";
+  task2[i].style.fontSize="45px";
+}
+console.log(task2,length)
+
+
+
+const task3 = document.querySelectorAll("#content p")
+For(let i=0;i<task3.length;i++){
+    task3[i].style;
+    backgroundColor="cyan"
+};
+

dfbb336109b6d495cbc9acf4454d5a3a2f8e8ae0
AkshayRao11 <akshay.khola.11@gmail.com>
2026-02-04 03:31:13 +0530

    todays commit

--- day 8 dom.js
+++ day 8 dom.js
@@ -1,22 +1,43 @@
-const heading = document.getElementById("myheading")
-console.log(heading);
+// Create a new element and set its content
+const newParagraph = document.createElement("p");
+newParagraph.textContent = "This is a dynamically created paragraph.";
+console.log(newParagraph);
+
+// Append the new element to the container
+const container = document.getElementById("container");
+container.appendChild(newParagraph);
+
+// remove Element
+//remove the first paragraph of container
+document.querySelector("#container p").remove();
 
 
-heading.style.color = "tomato";
-heading.style.backgroundColor = "yellow";
 
-const para = document.getElementsByClassName("para");
-console.log(para);
+const image = document.createElement("img");
 
-for(let i=0;i<para.length;i++){
-    para[i].style.backgroundColor="cyan";
+image.setAttribute("src","https://www.krmangalam.edu.in/wp-content/uploads/2024/02/178bs_Best-Colleges-for-Master-in-Commerce-in-Gurgaon-768x431-1.webp");
 
-}
+image.setAttribute("alt","K.R Mangalam");
+
+const gallery = document.getElementById("gallery");
+gallery.appendChild(image);
 
 
-//queryselector
+// Date method
 
-const qs =document.querySelector("#content p")
-console.log(qs)
-qs.textContent = "lorem djkaban. j d. eqjo. e j"
+let date = new Date()
+console.log(date);
 
+setInterval(()=>{
+let date = new Date()
+console.log(date);
+
+let hour = date.getHours() % 12 || 12;
+let min=date.getMinutes();
+let second= date.getSeconds();
+
+let clock = document.getElementById("digi-clock");
+
+clock.textContent=`${hour} : ${min} : ${second}`
+
+},1000)
